ca53882c7e298fde9999b2353821af558971b603
MildSevenFK <125388076+MildSevenFK@users.noreply.github.com>
2023-06-03 19:44:05 +0900

    BoardController CRUD완성, Service수정

--- src/main/java/com/nhnacademy/minidooray_gateway/controller/BoardController.java
+++ src/main/java/com/nhnacademy/minidooray_gateway/controller/BoardController.java
@@ -5,9 +5,10 @@ import com.nhnacademy.minidooray_gateway.dto.task.ProjectDto;
 import com.nhnacademy.minidooray_gateway.service.DoorayService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/dooray")
@@ -15,15 +16,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BoardController {
     private final DoorayService doorayService;
 
+    /**
+     * POST CRUD, BOARD 컨트롤러 작성
+     */
+
     @GetMapping("/board")
-    public String getBoardList(ProjectDto projectDto) {
-        doorayService.getProjectList(projectDto);
+    public String getBoardList(Model model) {
+        model.addAttribute(doorayService.getAllProjectList());
+        return "board";
+    }
+
+    @GetMapping("/board/{id}")
+    public String getBoardDetail(@PathVariable Long id, Model model) {
+        model.addAttribute("project", doorayService.getProjectDetail(id));
         return "board";
     }
 
     @PostMapping("/board")
-    public String postBoardList(ProjectDto projectDto) {
-        doorayService.getProjectList(projectDto);
+    public String postBoardList(@RequestBody ProjectDto projectDto) {
+        doorayService.saveProject(projectDto);
+        return "board";
+    }
+
+    @PutMapping("/board/{id}")
+    public String putBoardList(@PathVariable Long id, @RequestBody ProjectDto projectDto) {
+        doorayService.updateProject(id, projectDto);
+        return "board";
+    }
+
+    @DeleteMapping("/board/{id}")
+    public String deleteBoardList(@PathVariable Long id) {
+        doorayService.deleteProject(id);
         return "board";
     }
 
--- src/main/java/com/nhnacademy/minidooray_gateway/service/DoorayService.java
+++ src/main/java/com/nhnacademy/minidooray_gateway/service/DoorayService.java
@@ -3,10 +3,28 @@ package com.nhnacademy.minidooray_gateway.service;
 import com.nhnacademy.minidooray_gateway.dto.task.ProjectDto;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DoorayService {
 
-    public void getProjectList(ProjectDto projectDto) {
+    //#TODO: 실제 Account 및 서비스들과 연동 
+    
 
+    public ProjectDto getProjectDetail(Long id) {
+        return null;
+    }
+
+    public List<ProjectDto> getAllProjectList() {
+        return null;
+    }
+
+    public void saveProject(ProjectDto projectDto) {
+    }
+
+    public void updateProject(Long id, ProjectDto projectDto) {
+    }
+
+    public void deleteProject(Long id) {
     }
 }
